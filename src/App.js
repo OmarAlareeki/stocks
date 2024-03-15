3b304bb07655884367c59e7b2b74f1fca589ceda
OMAR <omarproductions2018@gmail.com>
2024-03-15 22:41:59 +0300

    landing page version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { ImCoinDollar } from "react-icons/im";
+import { IoMenu } from "react-icons/io5";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch(
+  //     "https://financialmodelingprep.com/api/v3/is-the-market-open?apikey=5JbedfpFQSiCRespheqYl9NO9BJHeHsG"
+  //   )
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     });
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ImCoinDollar className="App-logo" />
+        {/* <a href="https://www.flaticon.com/free-icons/precious" title="precious icons"></a> */}
+        <input />
+        <IoMenu className="header-menu" />
       </header>
+
+
+      {/* <div>
+        {data.results.map((dataItem) => (
+          // <span key={data.dataItem.request_id} width={100}>{dataItem}</span>
+        ))}
+      </div> */}
     </div>
   );
 }
