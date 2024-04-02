14b717062c0510f7ac6aa72bbb483b13a321f0e7
OMAR <omarproductions2018@gmail.com>
2024-04-03 00:04:38 +0300

    stockSearchPage,search,quote,companiesProfilePage, updated useFetch hook

---
+++ src/Hooks/axisFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+const useDataApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return [{ data, isLoading, isError }, setUrl];
+};
+
+export default useDataApi;
